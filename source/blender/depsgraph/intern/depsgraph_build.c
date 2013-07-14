cbaacb4e8aef509baa6e26f9f8c03e48d137b71b
Aligorith <aligorith@gmail.com>
2013-07-15 00:39:57 +1200

    Start of experimental code for building depsgraph
    Currently there's nothing here except for some includes and an entrypoint
    for the building process, starting from scene. Aim here is to start working
    on implementing this stuff to clarify a bit design issues of main API and
    datatypes, as any difficulties encountered during this process will end up
    revealing problem areas in the design (as well as giving us a more solid
    technical grounding when looking at things)

--- source/blender/depsgraph/intern/depsgraph_build.c
+++ source/blender/depsgraph/intern/depsgraph_build.c
@@ -26,3 +26,54 @@
  * Methods for constructing depsgraph
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "MEM_guardedalloc.h"
+
+#include "BLI_blenlib.h"
+#include "BLI_utildefines.h"
+
+#include "DNA_anim_types.h"
+#include "DNA_constraint_types.h"
+#include "DNA_material_types.h"
+#include "DNA_object_types.h"
+#include "DNA_scene_types.h"
+
+#include "BKE_constraint.h"
+#include "BKE_depsgraph.h"
+
+
+#include "depsgraph_types.h"
+#include "depsgraph_eval.h"
+
+
+/* ************************************************* */
+
+
+/* ************************************************* */
+/* Scene */
+
+/* build depsgraph for specified scene - this is called recursively for sets... */
+static DepsNode *deg_build_scene_graph(Depsgraph *graph, Scene *scene)
+{
+	/* build subgraph for set, and link this in... */
+	if (scene->set) {
+		DepsNode *set_node = deg_build_scene_graph(graph, scene->set);
+		// TODO: link set to scene, especially our timesource...
+	}
+}
+
+/* ************************************************* */
+/* Depsgraph Building Entrypoints */
+
+/* Build depsgraph for the given scene, and dump results in given graph container */
+// XXX: assume that this is called from outside, given the current scene as the "main" scene 
+void DEG_graph_build_from_scene(Depsgraph *graph, Scene *scene)
+{
+	deg_build_scene_graph(graph, scene);
+}
+
+/* ************************************************* */
+
