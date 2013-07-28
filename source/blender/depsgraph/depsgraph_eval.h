506782005cb5abc2a1fe729c1570697dd673f87b
Aligorith <aligorith@gmail.com>
2013-07-28 19:02:16 +1200

    Some operation contexts for most of the component types defined so far
    * This is just a rough first pass at what each of these should be like
    * TODO: Particles hasn't been done yet

--- source/blender/depsgraph/depsgraph_eval.h
+++ source/blender/depsgraph/depsgraph_eval.h
@@ -23,20 +23,92 @@
  *
  * ***** END GPL LICENSE BLOCK *****
  *
+ * "Operation Contexts" are used to pass state info (scene, parameter info, cfra)
+ * as well as the temporary data structure(s) that operations should perform their
+ * operations on. Thus, instead of operations potentially messing up state in places
+ * they shouldn't be touching, they are just provided with thread-safe micro-environments
+ * in which to work.
  */
 
 #ifndef __DEPSGRAPH_EVAL_TYPES_H__
 #define __DEPSGRAPH_EVAL_TYPES_H__
 
 /* ****************************************** */
+/* Operation Contexts */
 
+/* Generic Operations Context 
+ *
+ * This contains standard information that most/all
+ * operations will inevitably need at some point.
+ */
+typedef struct DEG_OperationsContext {
+	Main *bmain;          /* scene database to query data from (if needed) */
+	Scene *scene;         /* current scene we're working with */
 	
+	double cfra;          /* current frame (including subframe offset stuff) */
+	size_t type;          /* (eDepsNode_Type.OuterNodes) type of context (for debug purposes) */
+} DEG_OperationsContext;
 
+/* Component Contexts ========================= */
 
+/* Parameters */
+typedef struct DEG_ParametersContext {
+	DEG_OperationsContext ctx;       /* standard header */
 	
+	PointerRNA ptr;                  /* pointer to struct where parameters live */
+	// XXX: ptr to data instance?
+} DEG_ParametersContext;
 
 
+/* Animation */
+typedef struct DEG_AnimationContext {
+	DEG_OperationsContext ctx;       /* standard header */
+	
+	ID *id;                          /* ID block to evaluate AnimData for */
+	AnimData *adt;                   /* id->adt to be evaluated */
+	
+	// TODO: accumulation buffers for NLA?
+} DEG_AnimationContext;
 
+
+/* Transform */
+// XXX: assume for now that this is object's only...
+typedef struct DEG_TransformContext {
+	DEG_OperationsContext ctx;       /* standard header */
+	
+	float matrix[4][4];              /* 4x4 matrix where results go */
+	
+	Object *ob;                      /* object that we're evaluating */
+	struct bConstraintOb *cob;       /* constraint evaluation temp object/context */
+} DEG_TransformContext;
+
+
+/* Geometry */
+typedef struct DEG_GeometryContext {
+	DEG_OperationsContext ctx;      /* standard header */
+	
+	/* Output buffers - Only one of these should need to be used */
+	struct DerivedMesh *dm;         /* mesh output */
+	struct Displist *dl;            /* curves output */
+	struct Path *path;              /* parametric curve */  // XXX...
+	
+	/* Source Geometry */
+	ID *source;
+	
+	/* Assorted settings */
+	uint64_t customdata_mask;       /* customdata mask */
+} DEG_GeometryContext;
+
+
+/* Pose Evaluation */
+typedef struct DEG_PoseContext {
+	DEG_OperationsContext ctx;      /* standard header */
+	
+	/* Source Data */
+	/* NOTE: "iktrees" are stored on the bones as they're being evaluated... */
+	Object *ob;                     /* object that pose resides on */
+	bPose *pose;                    /* pose object that is being "solved" */
+} DEG_PoseContext;
 
 /* ****************************************** */
 
