e84a51fd7be8943f7e52b88d08994098946cf82b
Aligorith <aligorith@gmail.com>
2013-07-28 19:00:18 +1200

    Removing some more old crap

--- source/blender/depsgraph/depsgraph_eval.h
+++ source/blender/depsgraph/depsgraph_eval.h
@@ -23,51 +23,19 @@
  *
  * ***** END GPL LICENSE BLOCK *****
  *
- * Evaluation-Related Defines for Depsgraph
- *
- * The types defined here are for things like:
- * 1) how we represent data state and/or pass this on to the 
- *    evaluation functions at runtime, or 
- * 2) types which can be used for declaring automating 
- *    graph-building process
  */
 
 #ifndef __DEPSGRAPH_EVAL_TYPES_H__
 #define __DEPSGRAPH_EVAL_TYPES_H__
 
 /* ****************************************** */
-/* Evaluation State */
 
-// XXX: dummy... need to flesh this out
-// - stuff here for now is just rough draft of what might go here (based on ThreadedObjectUpdateState)
-typedef struct DegState {
-	ID *id;
-	void *data;
 	
-	SpinLock lock;
-} DegState;
 
-/* ****************************************** */
-/* Graph Build Types */
 
-/* Standard 3-step composition... 
- *
- * Examples (may not be actually done) of this include:
- * - Evaluating a single constraint
- */
-typedef struct sDegEvalTemplate_Standard {
-	char name[128];                       /* debugging identifier for this setup */
 	
-	DEG_AtomicEvalOperation_Cb init;	  /* initialisation (i.e. "preparation") step */
-	DEG_AtomicEvalOperation_Cb exec;      /* evaluation of data */
-	DEG_AtomicEvalOperation_Cb cleanup;   /* cleanup (i.e. "freeing") step */
-} sDegEvalTemplate_Standard;
 
-/* Standard 3-step where exec() step is actually a sub-graph... */
-// XXX...
 
-/* Arbitrarily many steps... */
-// XXX...
 
 
 /* ****************************************** */
