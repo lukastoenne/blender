a43c5a7ac4aceca50ee115612f9347497e6f24c6
Aligorith <aligorith@gmail.com>
2013-08-05 00:41:07 +1200

    Moved low-level node-finding code over to depsgraph_query.c

--- source/blender/depsgraph/intern/depsgraph_query.c
+++ source/blender/depsgraph/intern/depsgraph_query.c
@@ -151,6 +151,183 @@ DepsRelation DEG_copy_relation(const DepsRelation *src)
 }
 
 /* ************************************************ */
+/* Low-Level Querying API */
+/* NOTE: These querying operations are generally only
+ *       used internally within the Depsgraph module
+ *       and shouldn't really be exposed to the outside
+ *       world.
+ */
+
+/* Find Matching Node ------------------------------ */
+/* For situations where only a single matching node is expected 
+ * (i.e. mainly for use when constructing graph)
+ */
+
+/* helper for finding inner nodes by their names */
+static DepsNode *deg_find_inner_node(Depsgraph *graph, ID *id, eDepsNode_Type component_type, 
+                              eDepsNode_Type type, const char name[DEG_MAX_ID_NAME])
+{
+	ComponentDepsNode *component = (ComponentDepsNode *)DEG_find_node(graph, component_type, id, NULL);
+	
+	if (component) {
+		/* lookup node with matching name... */
+		DepsNode *node = BLI_ghash_lookup(component->op_hash, name);
+		
+		if (node) {
+			/* make sure type matches too... just in case */
+			BLI_assert(node->type == type);
+			return node;
+		}
+	}
+	
+	/* no match... */
+	return NULL;
+}
+
+/* helper for finding bone component nodes by their names */
+// XXX: cannot reliably find operation nodes by name, as we'd need 2 names!
+static DepsNode *deg_find_bone_component_node(Depsgraph *graph, ID *id, eDepsNode_Type type, const char name[DEG_MAX_ID_NAME])
+{
+	PoseComponentDepsNode *pose_comp = (PoseComponentDepsNode *)DEG_find_node(graph, DEPSNODE_TYPE_EVAL_POSE, id, NULL);
+	
+	if (pose_comp)  {
+		/* lookup bone component with matching name */
+		BoneComponentDepsNode *bone_node = BLI_ghash_lookup(pose_comp->bone_hash, name);
+		
+		if (type == DEPSNODE_TYPE_BONE) {
+			/* bone component is what we want */
+			return bone_node;
+		}
+		else if (type == DEPSNODE_TYPE_OP_BONE) {
+			/* assume for now that there's just a single operation node, and its name is exactly the same! */
+			return BLI_ghash_lookup(bone_node->op_hash, name);
+		}
+	}
+	
+	/* no match */
+	return NULL;
+}
+
+/* Find matching node */
+DepsNode *DEG_find_node(Depsgraph *graph, ID *id, eDepsNode_Type type, const char name[DEG_MAX_ID_NAME])
+{
+	DepsNode *result = NULL;
+	
+	/* each class of types requires a different search strategy... */
+	switch (type) {
+		/* "Generic" Types -------------------------- */
+		case DEPSNODE_TYPE_ROOT:   /* NOTE: this case shouldn't need to exist, but just in case... */
+			result = graph->root_node;
+			break;
+			
+		case DEPSNODE_TYPE_TIMESOURCE: /* Time Source */
+		{
+			/* search for one attached to a particular ID? */
+			if (id) {
+				/* check if it was added as a component 
+				 * (as may be done for subgraphs needing timeoffset) 
+				 */
+				// XXX: review this
+				IDDepsNode *id_node = BLI_ghash_lookup(graph->id_hash, id);
+				
+				if (id_node) {
+					result = BLI_ghash_lookup(id_node->component_hash,
+					                          SET_INT_IN_POINTER(type));
+				}
+			}
+			else {
+				/* use "official" timesource */
+				RootDepsNode *root_node = (RootDepsNode *)graph->root_node;
+				result = root_node->time_source;
+			}
+		}
+			break;
+		
+		case DEPSNODE_TYPE_ID_REF: /* ID Block Index/Reference */
+		{
+			/* lookup relevant ID using nodehash */
+			result = BLI_ghash_lookup(graph->id_hash, id);
+		}	
+			break;
+			
+		/* "Outer" Nodes ---------------------------- */
+		
+		case DEPSNODE_TYPE_PARAMETERS: /* Components... */
+		case DEPSNODE_TYPE_PROXY:
+		case DEPSNODE_TYPE_ANIMATION:
+		case DEPSNODE_TYPE_TRANSFORM:
+		case DEPSNODE_TYPE_GEOMETRY:
+		case DEPSNODE_TYPE_EVAL_POSE:
+		case DEPSNODE_TYPE_EVAL_PARTICLES:
+		{
+			/* Each ID-Node knows the set of components that are associated with it */
+			IDDepsNode *id_node = BLI_ghash_lookup(graph->id_hash, id);
+			
+			if (id_node) {
+				result = BLI_ghash_lookup(id_node->component_hash, type);
+			}
+		}
+			break;
+			
+		case DEPSNODE_TYPE_BONE:       /* Bone Component */
+		{
+			/* this will find the bone component */
+			result = deg_find_bone_node(graph, id, type, name);
+		}
+			break;
+		
+		/* "Inner" Nodes ---------------------------- */
+		
+		case DEPSNODE_TYPE_OP_PARAMETER:  /* Parameter Related Ops */
+			result = deg_find_inner_node(graph, id, DEPSNODE_TYPE_PARAMETERS, type, name);
+			break;
+		case DEPSNODE_TYPE_OP_PROXY:      /* Proxy Ops */
+			result = deg_find_inner_node(graph, id, DEPSNODE_TYPE_PROXY, type, name);
+			break;
+		case DEPSNODE_TYPE_OP_TRANSFORM:  /* Transform Ops */
+			result = deg_find_inner_node(graph, id, DEPSNODE_TYPE_TRANSFORM, type, name);
+			break;
+		case DEPSNODE_TYPE_OP_ANIMATION:  /* Animation Ops */
+			result = deg_find_inner_node(graph, id, DEPSNODE_TYPE_ANIMATION, type, name);
+			break;
+		case DEPSNODE_TYPE_OP_GEOMETRY:   /* Geometry Ops */
+			result = deg_find_inner_node(graph, id, DEPSNODE_TYPE_GEOMETRY, type, name);
+			break;
+			
+		case DEPSNODE_TYPE_OP_UPDATE:     /* Updates */
+			result = deg_find_inner_node(graph, id, DEPSNODE_TYPE_PARAMETERS, type, name);
+			break;
+		case DEPSNODE_TYPE_OP_DRIVER:     /* Drivers */
+			result = deg_find_inner_node(graph, id, DEPSNODE_TYPE_PARAMETERS, type, name);
+			break;
+			
+		case DEPSNODE_TYPE_OP_POSE:       /* Pose Eval (Non-Bone Operations) */
+			result = deg_find_inner_node(graph, id, DEPSNODE_TYPE_EVAL_POSE, type, name);
+			break;
+		case DEPSNODE_TYPE_OP_BONE:       /* Bone */
+			// XXX: this won't really work... this will only get us the bone component we want!
+			result = deg_find_bone_node(graph, id, type, name);
+			break;
+			
+		case DEPSNODE_TYPE_OP_PARTICLE:  /* Particle System/Step */
+			result = deg_find_inner_node(graph, id, DEPSNODE_TYPE_EVAL_PARTICLE, type, name);
+			break;
+			
+		case DEPSNODE_TYPE_OP_RIGIDBODY: /* Rigidbody Sim */
+			result = deg_find_inner_node(graph, id, DEPSNODE_TYPE_TRANSFORM, type, name); // XXX: needs review
+			break;
+		
+		default:
+			/* Unhandled... */
+			printf("%s(): Unknown node type %d\n", __func__, type);
+			break;
+	}
+	
+	return result;
+}
+
+
+/* ************************************************ */
 /* Querying API */
 
 /* ************************************************ */
