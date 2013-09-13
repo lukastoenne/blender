080e1385c760631e8fc80859a34b427fcc874497
Aligorith <aligorith@gmail.com>
2013-09-14 00:21:03 +1200

    Added a temporary include file for defining "stub" prototypes
    This is mainly intended for use during the porting period, when some of
    the necessary methods may not in fact exist in Blender for real yet.

---
+++ source/blender/depsgraph/stubs.h
@@ -0,0 +1,14 @@
+/* This file defines some stub function defs used 
+ * for various operation callbacks which have not
+ * been implemented in Blender yet. It should
+ * eventually disappear instead of remaining as
+ * part of the code base.
+ */
+
+#ifndef __DEPSGRAPH_FN_STUBS_H__
+#define __DEPSGRAPH_FN_STUBS_H__
+
+#pragma message("DEPSGRAPH PORTING XXX: There are still some undefined stubs")
+
+#endif //__DEPSGRAPH_FN_STUBS_H__
+
