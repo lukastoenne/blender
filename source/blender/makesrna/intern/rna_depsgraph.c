a9507a5f73ad245c45e418f1f21b1cf3e91a264d
Lukas Tönne <lukas.toenne@gmail.com>
2014-03-16 09:04:31 +0100

    Revert "Basic DepsNode type in RNA and a root_node property in the Depsgraph."
    This reverts commit 39b76f27bc4b8437bedb08dde46f968f81b8040f.
    
    The depsgraph requires only a minimal RNA representation as a means for
    debugging addons to access it and request debug dumps. Removing this
    also avoids internal header includes.

--- source/blender/makesrna/intern/rna_depsgraph.c
+++ source/blender/makesrna/intern/rna_depsgraph.c
@@ -29,7 +29,6 @@
 #include "BLI_utildefines.h"
 #include "BLI_path_util.h"
 
-#include "RNA_access.h"
 #include "RNA_define.h"
 #include "RNA_enum_types.h"
 
@@ -37,34 +36,9 @@
 
 #ifdef RNA_RUNTIME
 
-#include <string.h>
-
-#include "BLI_string.h"
-
 #include "DEG_depsgraph.h"
 #include "DEG_depsgraph_query.h"
 
-#include "intern/depsgraph_types.h"
-
-static void rna_DepsNode_name_get(PointerRNA *ptr, char *value)
-{
-	DepsNode *node = ptr->data;
-	strcpy(value, node->name);
-}
-
-static int rna_DepsNode_name_length(PointerRNA *ptr)
-{
-	DepsNode *node = ptr->data;
-	return strlen(node->name);
-}
-
-static PointerRNA rna_Depsgraph_root_node_get(PointerRNA *ptr)
-{
-	Depsgraph *graph = ptr->data;
-	PointerRNA root_node_ptr;
-	RNA_pointer_create(ptr->id.data, &RNA_DepsNode, graph->root_node, &root_node_ptr);
-	return root_node_ptr;
-}
 
 static void rna_Depsgraph_debug_graphviz(Depsgraph *graph, const char *filename)
 {
@@ -79,21 +53,6 @@ static void rna_Depsgraph_debug_graphviz(Depsgraph *graph, const char *filename)
 
 #else
 
-static void rna_def_depsnode(BlenderRNA *brna)
-{
-	StructRNA *srna;
-	PropertyRNA *prop;
-	
-	srna = RNA_def_struct(brna, "DepsNode", NULL);
-	RNA_def_struct_ui_text(srna, "Dependency Node", "");
-	
-	prop = RNA_def_property(srna, "name", PROP_STRING, PROP_NONE);
-	RNA_def_property_string_funcs(prop, "rna_DepsNode_name_get", "rna_DepsNode_name_length", NULL);
-	RNA_def_property_clear_flag(prop, PROP_EDITABLE);
-	RNA_def_property_ui_text(prop, "Name", "Identifier of the node");
-	RNA_def_struct_name_property(srna, prop);
-}
-
 static void rna_def_depsgraph(BlenderRNA *brna)
 {
 	StructRNA *srna;
@@ -104,10 +63,6 @@ static void rna_def_depsgraph(BlenderRNA *brna)
 	srna = RNA_def_struct(brna, "Depsgraph", NULL);
 	RNA_def_struct_ui_text(srna, "Dependency Graph", "");
 	
-	prop = RNA_def_property(srna, "root_node", PROP_POINTER, PROP_NONE);
-	RNA_def_property_struct_type(prop, "DepsNode");
-	RNA_def_property_pointer_funcs(prop, "rna_Depsgraph_root_node_get", NULL, NULL, NULL);
-	
 	func = RNA_def_function(srna, "debug_graphviz", "rna_Depsgraph_debug_graphviz");
 	parm = RNA_def_string_file_path(func, "filename", NULL, FILE_MAX, "File Name",
 	                                "File in which to store graphviz debug output");
@@ -116,7 +71,6 @@ static void rna_def_depsgraph(BlenderRNA *brna)
 
 void RNA_def_depsgraph(BlenderRNA *brna)
 {
-	rna_def_depsnode(brna);
 	rna_def_depsgraph(brna);
 }
 
