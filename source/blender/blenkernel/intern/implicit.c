854600aeed5d53736b8e009ae08c8872875aaa25
Lukas Tönne <lukas.toenne@gmail.com>
2014-09-05 12:48:49 +0200

    Alternative new solver for cloth using the Eigen CG solver instead of a custom built solver.
    The old cloth solver is broken unfortunately. Eigen is a designated
    linear algebra library and very likely their implementation is a lot
    better (can't compare until it's implemented though).
    
    Only basic gravity is active atm, spring forces, external force fields,
    damping and volumetric friction have to be added back by converting
    the data into the Eigen format.

--- source/blender/blenkernel/intern/implicit.c
+++ source/blender/blenkernel/intern/implicit.c
@@ -29,6 +29,9 @@
  *  \ingroup bke
  */
 
+//#define IMPLICIT_SOLVER_BLENDER
+
+#ifdef IMPLICIT_SOLVER_BLENDER
 
 #include "MEM_guardedalloc.h"
 
@@ -2477,3 +2480,5 @@ void implicit_set_positions(ClothModifierData *clmd)
 	if (G.debug_value > 0)
 		printf("implicit_set_positions\n");
 }
+
+#endif /* IMPLICIT_SOLVER_BLENDER */
