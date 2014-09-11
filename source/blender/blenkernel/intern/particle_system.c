6cd7d8f0ec4bc64edb37b22e33405852e8c2de85
Lukas Tönne <lukas.toenne@gmail.com>
2014-09-11 14:19:52 +0200

    Added new data in the cloth modifier for hair root information.
    This will allow us to implement moving reference frames for hair and
    make "fictitious" forces optional, aiding in creating stable and
    controllable hair systems.
    
    Adding data in this place is a nasty hack, but it's too difficult to
    encode as a DM data layer and the whole cloth modifier/DM intermediate
    data copying for hair should be removed anyway.

--- source/blender/blenkernel/intern/particle_system.c
+++ source/blender/blenkernel/intern/particle_system.c
@@ -4028,11 +4028,18 @@ static void do_hair_dynamics(ParticleSimulationData *sim)
 	if (dm && (totpoint != dm->getNumVerts(dm) || totedge != dm->getNumEdges(dm))) {
 		dm->release(dm);
 		dm = psys->hair_in_dm = NULL;
+		
+		MEM_freeN(psys->clmd->roots);
+		psys->clmd->roots = NULL;
 	}
 
 	if (!dm) {
 		dm = psys->hair_in_dm = CDDM_new(totpoint, totedge, 0, 0, 0);
 		DM_add_vert_layer(dm, CD_MDEFORMVERT, CD_CALLOC, NULL);
+	}
+	
+	if (!psys->clmd->roots) {
+		psys->clmd->roots = MEM_mallocN(sizeof(ClothHairRoot) * totpoint, "hair roots");
 	}
 
 	mvert = CDDM_get_verts(dm);
@@ -4050,6 +4057,7 @@ static void do_hair_dynamics(ParticleSimulationData *sim)
 		psys_mat_hair_to_object(sim->ob, sim->psmd->dm, psys->part->from, pa, hairmat);
 
 		for (k=0, key=pa->hair; k<pa->totkey; k++,key++) {
+			ClothHairRoot *root = &psys->clmd->roots[pa->hair_index + k - 1];
 			
 			/* create fake root before actual root to resist bending */
 			if (k==0) {
@@ -4074,6 +4082,10 @@ static void do_hair_dynamics(ParticleSimulationData *sim)
 					dvert++;
 				}
 			}
+
+			/* store root transform in cloth data */
+			copy_v3_v3(root->loc, hairmat[3]);
+			copy_m3_m4(root->rot, hairmat);
 
 			copy_v3_v3(mvert->co, key->co);
 			mul_m4_v3(hairmat, mvert->co);
