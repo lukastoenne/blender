bd2bef2276d60a883c77d985c8e3d10edb47da61
Lukas Tönne <lukas.toenne@gmail.com>
2015-02-03 16:14:07 +0100

    Register the particle modifier operators.

--- source/blender/editors/physics/physics_ops.c
+++ source/blender/editors/physics/physics_ops.c
@@ -69,6 +69,10 @@ static void operatortypes_particle(void)
 	WM_operatortype_append(PARTICLE_OT_particle_edit_toggle);
 	WM_operatortype_append(PARTICLE_OT_edited_clear);
 
+	WM_operatortype_append(PARTICLE_OT_modifier_add);
+	WM_operatortype_append(PARTICLE_OT_modifier_remove);
+	WM_operatortype_append(PARTICLE_OT_modifier_move_up);
+	WM_operatortype_append(PARTICLE_OT_modifier_move_down);
 
 	WM_operatortype_append(OBJECT_OT_particle_system_add);
 	WM_operatortype_append(OBJECT_OT_particle_system_remove);
