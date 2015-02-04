48954596d109d606679ea08d54098dd269c42280
Antony Riakiotakis <kalast@gmail.com>
2015-02-04 13:35:56 +0100

    Disable sequencer preprocess cache if 314 debug value is set

--- source/blender/blenkernel/intern/sequencer.c
+++ source/blender/blenkernel/intern/sequencer.c
@@ -2934,7 +2934,9 @@ static ImBuf *seq_render_strip(const SeqRenderData *context, Sequence *seq, floa
 		ibuf = copy_from_ibuf_still(context, seq, nr);
 
 		if (ibuf == NULL) {
-			ibuf = BKE_sequencer_preprocessed_cache_get(context, seq, cfra, SEQ_STRIPELEM_IBUF);
+			/* disable caching in that case */
+			if (G.debug_value != 314)
+				ibuf = BKE_sequencer_preprocessed_cache_get(context, seq, cfra, SEQ_STRIPELEM_IBUF);
 
 			if (ibuf == NULL) {
 				/* MOVIECLIPs have their own proxy management */
