56e246903003df46d3af067113522e867538bf80
Antony Riakiotakis <kalast@gmail.com>
2015-02-06 17:22:08 +0100

    Cleanup

--- source/blender/editors/space_sequencer/sequencer_edit.c
+++ source/blender/editors/space_sequencer/sequencer_edit.c
@@ -2460,16 +2460,16 @@ static int sequencer_meta_toggle_exec(bContext *C, wmOperator *UNUSED(op))
 	MetaStack *ms;
 
 	if (last_seq && last_seq->type == SEQ_TYPE_META && last_seq->flag & SELECT) {
-			/* Enter Metastrip */
-			ms = MEM_mallocN(sizeof(MetaStack), "metastack");
-			BLI_addtail(&ed->metastack, ms);
-			ms->parseq = last_seq;
-			ms->oldbasep = ed->seqbasep;
-			copy_v2_v2_int(ms->disp_range, &ms->parseq->startdisp);
+		/* Enter Metastrip */
+		ms = MEM_mallocN(sizeof(MetaStack), "metastack");
+		BLI_addtail(&ed->metastack, ms);
+		ms->parseq = last_seq;
+		ms->oldbasep = ed->seqbasep;
+		copy_v2_v2_int(ms->disp_range, &ms->parseq->startdisp);
 
-			ed->seqbasep = &last_seq->seqbase;
+		ed->seqbasep = &last_seq->seqbase;
 
-			BKE_sequencer_active_set(scene, NULL);
+		BKE_sequencer_active_set(scene, NULL);
 	}
 	/* scene strip */
 	else if (last_seq && last_seq->type == SEQ_TYPE_SCENE && last_seq->scene &&
