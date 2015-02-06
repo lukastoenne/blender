94e97424534ca74aa1d9e3eb081fb315a365940d
Lukas Tönne <lukas.toenne@gmail.com>
2015-02-06 12:18:32 +0100

    Print actual changed paths in the "make paths absolute" operator.

--- source/blender/blenkernel/intern/bpath.c
+++ source/blender/blenkernel/intern/bpath.c
@@ -164,6 +164,7 @@ static bool bpath_absolute_convert_visit_cb(void *userdata, char *path_dst, cons
 		strcpy(path_dst, path_src);
 		BLI_path_abs(path_dst, data->basedir);
 		if (BLI_path_is_rel(path_dst) == false) {
+			BKE_reportf(data->reports, RPT_INFO, "Path '%s' made absolute to '%s'", path_src, path_dst);
 			data->count_changed++;
 		}
 		else {
