136c8d1ae0fafc90281bd778c3ce362bf7837694
Sergey Sharybin <sergey.vfx@gmail.com>
2015-02-19 00:00:23 +0500

    Add argument to DM_to_mesh() function to take ownership over the DM
    The idea is pretty simple: instead of making temporary copy of all the
    related custom data layers just pass the ownership from the DM to the
    mesh.
    
    This is really handy in cases when you've got DM which you need to
    convert to Mesh datablock and wouldn't need that DM after conversion
    anyway.
    
    Foe example, render database conversion, exporters and even Modifier
    Apply will benefit from this option.
    
    Reviewers: campbellbarton
    
    Differential Revision: https://developer.blender.org/D1127

--- source/blender/blenkernel/intern/customdata.c
+++ source/blender/blenkernel/intern/customdata.c
@@ -1559,6 +1559,25 @@ void CustomData_free(CustomData *data, int totelem)
 	CustomData_reset(data);
 }
 
+void CustomData_free_typemask(struct CustomData *data, int totelem, CustomDataMask mask)
+{
+	int i;
+
+	for (i = 0; i < data->totlayer; ++i) {
+		CustomDataLayer *layer = &data->layers[i];
+		if (!(mask & CD_TYPE_AS_MASK(layer->type))) {
+			continue;
+		}
+		customData_free_layer__internal(layer, totelem);
+	}
+
+	if (data->layers)
+		MEM_freeN(data->layers);
+
+	CustomData_external_free(data);
+	CustomData_reset(data);
+}
+
 static void customData_update_offsets(CustomData *data)
 {
 	const LayerTypeInfo *typeInfo;
