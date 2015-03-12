31009f8919ad52d10960013cf68e28e9720edaa9
Lukas Tönne <lukas.toenne@gmail.com>
2015-03-12 17:54:13 +0100

    Use a cached group only when a CacheLibrary is actually available.
    Otherwise the regular duplilist generator is used.

--- source/blender/blenkernel/intern/cache_library.c
+++ source/blender/blenkernel/intern/cache_library.c
@@ -1209,6 +1209,18 @@ bool BKE_cache_read_particles_pathcache_children(Main *bmain, Scene *scene, floa
 	return false;
 }
 
+bool BKE_cache_test_dupligroup(Main *bmain, eCacheLibrary_EvalMode eval_mode, struct Group *dupgroup)
+{
+	CacheLibrary *cachelib;
+	
+	FOREACH_CACHELIB_READ(bmain, cachelib, eval_mode) {
+		if (cachelib->group == dupgroup) {
+			return true;
+		}
+	}
+	return false;
+}
+
 bool BKE_cache_read_dupligroup(Main *bmain, Scene *scene, float frame, eCacheLibrary_EvalMode eval_mode, struct Group *dupgroup, struct DupliCache *dupcache)
 {
 	CacheLibrary *cachelib;
