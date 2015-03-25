d92b6cba6798d2120497e160524bcd84c8b8bff5
Lukas Tönne <lukas.toenne@gmail.com>
2015-03-25 14:46:30 +0100

    Revert "Disable cuda binaries on the buildbot for the gooseberry branch."
    This reverts commit 83da7b1edba27aaeef0a6d4e3114390305f7dc02.

--- build_files/buildbot/config/user-config-cuda-glibc211-x86_64.py
+++ build_files/buildbot/config/user-config-cuda-glibc211-x86_64.py
@@ -2,6 +2,4 @@ BF_BUILDDIR = '../blender-build/linux-glibc211-x86_64'
 BF_INSTALLDIR = '../blender-install/linux-glibc211-x86_64'
 BF_NUMJOBS = 1
 
-# XXX disabled cuda bins for gooseberry branch for faster builds
-#BF_CYCLES_CUDA_BINARIES_ARCH = ['sm_20', 'sm_21', 'sm_30', 'sm_35', 'sm_50', 'sm_52']
-BF_CYCLES_CUDA_BINARIES_ARCH = []
+BF_CYCLES_CUDA_BINARIES_ARCH = ['sm_20', 'sm_21', 'sm_30', 'sm_35', 'sm_50', 'sm_52']
