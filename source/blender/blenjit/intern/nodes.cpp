1d038a520bb3c88573a27790b919178f281ddadc
Lukas Tönne <lukas.toenne@gmail.com>
2015-07-04 19:06:25 +0200

    Node tree builder class to help convert a DNA bNodeTree into the JIT code generator representation.

--- source/blender/blenjit/intern/nodes.cpp
+++ source/blender/blenjit/intern/nodes.cpp
@@ -31,12 +31,24 @@
 
 #include <map>
 #include <string>
+#include <vector>
 
+extern "C" {
+#include "BLI_utildefines.h"
+
+#include "DNA_node_types.h"
+
+#include "BKE_node.h"
+}
+
+#include "BJIT_nodes.h"
 #include "bjit_intern.h"
 
 namespace bjit {
 
 using namespace llvm;
+
+struct NodeType;
 
 struct NodeSocket {
 	NodeSocket(const std::string &name) :
@@ -58,16 +70,41 @@ struct NodeType {
 	~NodeType()
 	{}
 	
+	const NodeSocket *find_input(const std::string &name) const
+	{
+		SocketMap::const_iterator it = inputs.find(name);
+		return (it != inputs.end())? &it->second : NULL;
+	}
+	
+	const NodeSocket *find_output(const std::string &name) const
+	{
+		SocketMap::const_iterator it = outputs.find(name);
+		return (it != outputs.end())? &it->second : NULL;
+	}
+	
 	std::string name;
 	SocketMap inputs;
 	SocketMap outputs;
 };
 
 struct NodeInstance {
-	typedef std::map<std::string, Value *> SocketMap;
-	typedef std::pair<std::string, Value *> SocketMapPair;
 	typedef NodeType::SocketMap SocketTypeMap;
-	typedef NodeType::SocketMapPair SocketTypeMapPair;
+	typedef NodeType::SocketMapPair SocketTypePair;
+	
+	struct InputInstance {
+		NodeInstance *link_node;
+		const NodeSocket *link_socket;
+		Value *value;
+	};
+	
+	struct OutputInstance {
+		Value *value;
+	};
+	
+	typedef std::map<std::string, InputInstance> InputMap;
+	typedef std::pair<std::string, InputInstance> InputPair;
+	typedef std::map<std::string, OutputInstance> OutputMap;
+	typedef std::pair<std::string, OutputInstance> OutputPair;
 	
 	NodeInstance(const NodeType *type, const std::string &name) :
 	    type(type), name(name)
@@ -76,60 +113,82 @@ struct NodeInstance {
 	~NodeInstance()
 	{}
 	
+	bool set_input_value(const std::string &name, Value *value)
+	{
+		InputInstance &input = inputs[name];
+		if (input.value)
+			return false;
+		input.value = value;
+		return true;
+	}
+	
+	bool set_input_link(const std::string &name, NodeInstance *from_node, const NodeSocket *from_socket)
+	{
+		InputInstance &input = inputs[name];
+		if (input.link_node || input.link_socket)
+			return false;
+		
+		input.link_node = from_node;
+		input.link_socket = from_socket;
+		return true;
+	}
+	
 	const NodeType *type;
 	std::string name;
-	SocketMap inputs;
-	SocketMap outputs;
+	InputMap inputs;
+	OutputMap outputs;
 	CallInst *call_inst;
 };
 
-struct NodeLink {
-	NodeLink(NodeInstance *from_node, NodeSocket *from_socket, NodeInstance *to_node, NodeSocket *to_socket) :
-	    from_node(from_node), from_socket(from_socket), to_node(to_node), to_socket(to_socket)
-	{}
-	~NodeLink()
-	{}
-	
-	NodeInstance *from_node;
-	NodeSocket *from_socket;
-	NodeInstance *to_node;
-	NodeSocket *to_socket;
-};
-
-template <typename Traits>
 struct NodeTree {
 	typedef std::map<std::string, NodeType> NodeTypeMap;
 	typedef std::pair<std::string, NodeType> NodeTypeMapPair;
 	typedef std::map<std::string, NodeInstance> NodeInstanceMap;
 	typedef std::pair<std::string, NodeInstance> NodeInstanceMapPair;
 	
-	NodeTree()
-	{}
-	~NodeTree()
-	{}
+	static NodeTypeMap node_types;
 	
-	static NodeType *find_node_type(const std::string &name)
+	static const NodeType *find_node_type(const std::string &name)
 	{
 		NodeTypeMap::const_iterator it = node_types.find(name);
 		return (it != node_types.end())? &it->second : NULL;
 	}
+	
+	NodeTree()
+	{}
+	~NodeTree()
+	{}
 	
 	NodeInstance *get_node(const std::string &name)
 	{
 		NodeInstanceMap::iterator it = nodes.find(name);
 		return (it != nodes.end())? &it->second : NULL;
 	}
-
+	
 	NodeInstance *add_node(const std::string &type, const std::string &name)
 	{
-		NodeType *nodetype = find_node_type(type);
+		const NodeType *nodetype = find_node_type(type);
 		std::pair<NodeInstanceMap::iterator, bool> result =
 		        nodes.insert(NodeInstanceMapPair(name, NodeInstance(nodetype, name)));
 		
 		return (result.second)? &result.first->second : NULL;
 	}
 	
-	static NodeTypeMap node_types;
+	bool add_link(const std::string &from_node, const std::string &from_socket,
+	              const std::string &to_node, const std::string &to_socket)
+	{
+		NodeInstance *tonode = get_node(to_node);
+		NodeInstance *fromnode = get_node(from_node);
+		const NodeSocket *fromsocket = fromnode->type->find_output(from_socket);
+		
+		if (!tonode || !fromnode || !fromsocket)
+			return false;
+		
+		tonode->set_input_link(to_socket, fromnode, fromsocket);
+		
+		return true;
+	}
+	
 	NodeInstanceMap nodes;
 };
 
@@ -138,8 +197,75 @@ struct NodeTree {
 template <typename T>
 struct NodeTreeBuilder;
 
+template <>
+struct NodeTreeBuilder<bNodeTree> {
+	NodeTree build(bNodeTree *btree)
+	{
+		NodeTree tree;
+		
+		for (bNode *bnode = (bNode*)btree->nodes.first; bnode; bnode = bnode->next) {
+			BLI_assert(bnode->typeinfo != NULL);
+			if (!nodeIsRegistered(bnode))
+				continue;
+			
+			const char *type = bnode->typeinfo->idname;
+			/*NodeInstance *node =*/ tree.add_node(type, bnode->name);
+		}
+		
+		for (bNodeLink *blink = (bNodeLink *)btree->links.first; blink; blink = blink->next) {
+			if (!(blink->flag & NODE_LINK_VALID))
+				continue;
+			
+			tree.add_link(blink->fromnode->name, blink->fromsock->name,
+			              blink->tonode->name, blink->tosock->name);
+		}
+		
+		return tree;
+	}
+};
 
-//template <>
-//struct NodeTreeBuilder<bNodeTree>;
+
+template <typename T>
+struct ListBaseIterator {
+	ListBaseIterator() :
+	    link(NULL)
+	{}
+
+	ListBaseIterator(const ListBase &lb) :
+	    link((Link *)lb.first)
+	{}
+	
+	ListBaseIterator& operator++ (void)
+	{
+		link = link->next;
+		return *this;
+	}
+	
+	T* operator* (void)
+	{
+		return (T *)link;
+	}
+	
+private:
+	Link *link;
+};
 
 } /* namespace bjit */
+
+/* ------------------------------------------------------------------------- */
+
+using namespace bjit;
+
+void *BJIT_build_nodetree_function(bNodeTree *ntree)
+{
+	NodeTreeBuilder<bNodeTree> builder;
+	
+	NodeTree tree = builder.build(ntree);
+	
+	return NULL;
+}
+
+void BJIT_free_nodetree_function(void *func)
+{
+	
+}
