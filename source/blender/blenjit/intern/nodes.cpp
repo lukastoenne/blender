3cd3621ddb6980294cfd89aa1c899e696ba93776
Lukas Tönne <lukas.toenne@gmail.com>
2015-07-05 14:22:43 +0200

    NodeTreeBuilder variant that handles effector lists.
    The "tree" structure (to be renamed, legacy habit) is flexible enough to describe
    all sorts of higher-level functional combinations: node networks, stacks, or
    sums of arbitrary influences as effectors do.

--- source/blender/blenjit/intern/nodes.cpp
+++ source/blender/blenjit/intern/nodes.cpp
@@ -37,7 +37,10 @@ extern "C" {
 #include "BLI_utildefines.h"
 
 #include "DNA_node_types.h"
+#include "DNA_object_force.h"
+#include "DNA_object_types.h"
 
+#include "BKE_effect.h"
 #include "BKE_node.h"
 }
 
@@ -50,6 +53,7 @@ using namespace llvm;
 
 struct NodeType;
 
+
 struct NodeSocket {
 	NodeSocket(const std::string &name) :
 	    name(name)
@@ -61,8 +65,7 @@ struct NodeSocket {
 };
 
 struct NodeType {
-	typedef std::map<std::string, NodeSocket> SocketMap;
-	typedef std::pair<std::string, NodeSocket> SocketMapPair;
+	typedef std::vector<NodeSocket> SocketList;
 	
 	NodeType(const std::string &name) :
 	    name(name)
@@ -70,27 +73,39 @@ struct NodeType {
 	~NodeType()
 	{}
 	
+	const NodeSocket *find_input(int index) const { return &inputs[index]; }
+	const NodeSocket *find_output(int index) const { return &outputs[index]; }
+	
 	const NodeSocket *find_input(const std::string &name) const
 	{
-		SocketMap::const_iterator it = inputs.find(name);
-		return (it != inputs.end())? &it->second : NULL;
+		for (SocketList::const_iterator it = inputs.begin(); it != inputs.end(); ++it) {
+			const NodeSocket &socket = *it;
+			if (socket.name == name)
+				return &socket;
+		}
+		return NULL;
 	}
 	
 	const NodeSocket *find_output(const std::string &name) const
 	{
-		SocketMap::const_iterator it = outputs.find(name);
-		return (it != outputs.end())? &it->second : NULL;
+		for (SocketList::const_iterator it = outputs.begin(); it != outputs.end(); ++it) {
+			const NodeSocket &socket = *it;
+			if (socket.name == name)
+				return &socket;
+		}
+		return NULL;
 	}
 	
+	/* stub implementation in case socket is passed directly */
+	const NodeSocket *find_input(const NodeSocket *socket) const { return socket; }
+	const NodeSocket *find_output(const NodeSocket *socket) const { return socket; }
+	
 	std::string name;
-	SocketMap inputs;
-	SocketMap outputs;
+	SocketList inputs;
+	SocketList outputs;
 };
 
 struct NodeInstance {
-	typedef NodeType::SocketMap SocketTypeMap;
-	typedef NodeType::SocketMapPair SocketTypePair;
-	
 	struct InputInstance {
 		NodeInstance *link_node;
 		const NodeSocket *link_socket;
@@ -174,28 +189,40 @@ struct NodeTree {
 		return (result.second)? &result.first->second : NULL;
 	}
 	
-	bool add_link(const std::string &from_node, const std::string &from_socket,
-	              const std::string &to_node, const std::string &to_socket)
+	template <typename FromT, typename ToT>
+	bool add_link(NodeInstance *from_node, FromT from,
+	              NodeInstance *to_node, ToT to)
 	{
-		NodeInstance *tonode = get_node(to_node);
-		NodeInstance *fromnode = get_node(from_node);
-		const NodeSocket *fromsocket = fromnode->type->find_output(from_socket);
-		
-		if (!tonode || !fromnode || !fromsocket)
+		if (!to_node || !from_node)
 			return false;
 		
-		tonode->set_input_link(to_socket, fromnode, fromsocket);
+		const NodeSocket *from_socket = from_node->type->find_output(from);
+		const NodeSocket *to_socket = to_node->type->find_input(to);
+		if (!from_socket || !to_socket)
+			return false;
+		
+		to_node->set_input_link(to_socket->name, from_node, from_socket);
 		
 		return true;
+	}
+	
+	template <typename FromT, typename ToT>
+	bool add_link(const std::string &from_node, FromT from,
+	              const std::string &to_node, ToT to)
+	{
+		return add_link(get_node(from_node), from, get_node(to_node), to);
 	}
 	
 	NodeInstanceMap nodes;
 };
 
-/* ------------------------------------------------------------------------- */
+/* ========================================================================= */
 
 template <typename T>
 struct NodeTreeBuilder;
+
+/* ------------------------------------------------------------------------- */
+/* bNodeTree */
 
 template <>
 struct NodeTreeBuilder<bNodeTree> {
@@ -218,6 +245,85 @@ struct NodeTreeBuilder<bNodeTree> {
 			
 			tree.add_link(blink->fromnode->name, blink->fromsock->name,
 			              blink->tonode->name, blink->tosock->name);
+		}
+		
+		return tree;
+	}
+};
+
+/* ------------------------------------------------------------------------- */
+/* Effectors */
+
+static std::string get_effector_prefix(short forcefield)
+{
+	switch (forcefield) {
+		case PFIELD_FORCE:
+			return "force";
+		case PFIELD_WIND:
+			return "wind";
+			
+		case PFIELD_NULL:
+		case PFIELD_VORTEX:
+		case PFIELD_MAGNET:
+		case PFIELD_GUIDE:
+		case PFIELD_TEXTURE:
+		case PFIELD_HARMONIC:
+		case PFIELD_CHARGE:
+		case PFIELD_LENNARDJ:
+		case PFIELD_BOID:
+		case PFIELD_TURBULENCE:
+		case PFIELD_DRAG:
+		case PFIELD_SMOKEFLOW:
+			return "";
+		
+		default: {
+			/* unknown type, should not happen */
+			BLI_assert(false);
+			return "";
+		}
+	}
+	return "";
+}
+
+template <>
+struct NodeTreeBuilder<EffectorContext> {
+	NodeTree build(EffectorContext *effctx)
+	{
+		NodeTree tree;
+		NodeInstance *node_prev = NULL;
+		const NodeSocket *socket_prev = NULL;
+		
+		for (EffectorCache *eff = (EffectorCache *)effctx->effectors.first; eff; eff = eff->next) {
+			if (!eff->ob || !eff->pd)
+				continue;
+			
+			std::string prefix = get_effector_prefix(eff->pd->forcefield);
+			if (prefix.empty()) {
+				/* undefined force type */
+				continue;
+			}
+			
+			std::string nodetype = "effector_" + prefix + "_eval";
+			std::string nodename = eff->ob->id.name;
+			NodeInstance *node = tree.add_node(nodetype, nodename);
+			const NodeSocket *socket = node->type->find_output(0);
+			
+			if (node_prev && socket_prev) {
+				std::string combinename = "combine_" + node_prev->name + "_" + node->name;
+				NodeInstance *combine = tree.add_node("effector_result_combine", combinename);
+				
+				tree.add_link(node_prev, socket_prev,
+				              combine, combine->type->find_input(0));
+				tree.add_link(node, socket,
+				              combine, combine->type->find_input(1));
+				
+				node_prev = combine;
+				socket_prev = combine->type->find_output(0);
+			}
+			else {
+				node_prev = node;
+				socket_prev = socket;
+			}
 		}
 		
 		return tree;
