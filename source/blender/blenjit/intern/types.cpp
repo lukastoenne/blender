f2076459909f553454ac50d0ce70879794914cbb
Lukas Tönne <lukas.toenne@gmail.com>
2015-07-12 17:00:06 +0200

    Moved code for socket types into the types header.

--- source/blender/blenjit/intern/types.cpp
+++ source/blender/blenjit/intern/types.cpp
@@ -36,4 +36,27 @@ using namespace llvm;
 
 namespace bjit {
 
+template <SocketTypeID type>
+struct SocketTypeGetter {
+	static Type *get(SocketTypeID t, LLVMContext &context)
+	{
+		if (t == type)
+			return TypeBuilder<typename SocketTypeImpl<type>::type, true>::get(context);
+		return SocketTypeGetter<(SocketTypeID)((int)type + 1)>::get(t, context);
+	}
+};
+
+template <>
+struct SocketTypeGetter<BJIT_NUMTYPES> {
+	static Type *get(SocketTypeID /*t*/, LLVMContext &/*context*/)
+	{
+		return NULL;
+	}
+};
+
+Type *bjit_get_socket_llvm_type(SocketTypeID type, LLVMContext &context)
+{
+	return SocketTypeGetter<(SocketTypeID)0>::get(type, context);
+}
+
 } /* namespace bjit */
